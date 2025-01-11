c4ccdf87cf6a3fa4e073039a3905e71bde75d77a
Asadbek <71205691+AsadbekAbdullayev@users.noreply.github.com>
2025-01-11 16:49:52 +0500

    Update index.jsx

--- src/component/UI/Crud_home/index.jsx
+++ src/component/UI/Crud_home/index.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Input, Select, Button, message, Popconfirm, Empty } from 'antd';
 
-
 const { TextArea } = Input;
 const { Option } = Select;
 
