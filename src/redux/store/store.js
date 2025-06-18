50336929bb7dac5680296cf58322f1b357192512
Asadbek <71205691+AsadbekAbdullayev@users.noreply.github.com>
2025-06-18 12:51:05 +0500

    Update store.js

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,19 +1,6 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import generelReducer from '../slices/generelSlice';
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 const reducers = combineReducers({
 	generelReducer,
 });
