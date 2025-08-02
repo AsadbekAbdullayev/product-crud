b408284eba96f645078c4843296085a8d499b7a7
Asadbek <71205691+AsadbekAbdullayev@users.noreply.github.com>
2025-08-02 15:28:36 +0500

    Update index.jsx

--- src/component/UI/Crud_home/index.jsx
+++ src/component/UI/Crud_home/index.jsx
@@ -3,12 +3,6 @@ import { Input, Select, Button, message, Popconfirm, Empty } from 'antd';
 const { TextArea } = Input;
 const { Option } = Select;
 
-
-
-
-
-
-
 const CrudHome = () => {
 	const [products, setProducts] = useState([]);
 	const [categories, setCategories] = useState([]);
