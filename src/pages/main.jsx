8fdd20cd26a40971718f6b7251c1b8b5fd9c7cbc
Asadbek <71205691+AsadbekAbdullayev@users.noreply.github.com>
2025-09-08 17:40:33 +0500

    Update main.jsx

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import HomeComponent from '@/component/UI/Crud_home';
 
-const Main = () => {
-	return <HomeComponent />;
-};
+const Main: React.FC = () => <HomeComponent />;
 
 export default Main;
+
