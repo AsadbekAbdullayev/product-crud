68ca02601ec519f74449d1d6df1cc8c510fce657
Asadbek <71205691+AsadbekAbdullayev@users.noreply.github.com>
2025-10-04 11:38:31 +0500

    simplify useLocation

--- src/component/Loader/Loader.jsx
+++ src/component/Loader/Loader.jsx
@@ -8,13 +8,12 @@ interface LoaderProps {
 }
 
 export const Loader: React.FC<LoaderProps> = ({ size = 62 }) => {
-	const { pathname } = useLocation();
-	const isAdmin = pathname.includes('admin');
+	const isAdmin = useLocation().pathname.includes('admin');
 
 	return (
 		<div
 			className={`w-full h-screen flex items-center justify-center ${
-				isAdmin ? 'bg-black bg-opacity-10' : 'bg-white'
+				isAdmin ? 'bg-black/10' : 'bg-white'
 			}`}
 		>
 			<Spin
